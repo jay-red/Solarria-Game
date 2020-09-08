95e571cd4f10dadb3ab70dbd5e66147f2392c3e5
Jared Flores <jared_flores@cs.ucsb.edu>
2020-09-08 02:27:44 -0700

    wucky

--- docs/js/client.js
+++ docs/js/client.js
@@ -1,6 +1,6 @@
 function init_client( cb_open, cb_msg ) {
 	var RTC_CFG = { 
-			iceServers : [ { urls : "stun:stun2.1.google.com:19302" } ] 
+			iceServers : [ { urls : "stun:stun.l.google.com:19302" } ] 
 		},
 		RTC_OPT = {
 			optional : [ { RtpDataChannels : false } ]
